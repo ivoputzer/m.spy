ee2346b1c7b09e0b8feec400c05a515ca645c231
Alessio Coser <alessio.coser@xpeppers.com>
2018-10-03 11:05:25 +0200

    improves .returned(arg) equality method for objects and arrays

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-exports.spy = (realFn) => {
+function deepEqual(expected, actual) {
+  let expectedType = typeof expected
+
+  if (!expected || !actual) return false
+  if (expectedType !== typeof actual) return false
+
+  if (expectedType === 'object') {
+    return Object.keys(expected).length === Object.keys(actual).length &&
+      Object.keys(expected).every(key => deepEqual(expected[key], actual[key]))
+  }
+
+  if (expectedType === 'array') {
+    return expected.length === actual.length &&
+           expected.every(exp, index => deepEqual(exp, actual[index]))
+  }
+
+  return expected === actual
+}
+
+exports.spy = (spiedFn) => {
   const fn = (...args) => {
     fn.called = true
     fn.notCalled = false
     fn.args.push(args)
     fn.callCount = fn.args.length
 
-    if (typeof realFn === 'function') {
-      let realReturn = realFn(...args)
-      fn.returns.push(realReturn)
-      return realReturn
+    if (typeof spiedFn === 'function') {
+      let spiedReturn = spiedFn(...args)
+      fn.returns.push(spiedReturn)
+      return spiedReturn
     }
 
     return null
@@ -18,7 +37,7 @@ exports.spy = (realFn) => {
   fn.callCount = 0
   fn.args = []
   fn.returns = []
-  fn.returned = (expected) => fn.returns.some(returned => expected === returned)
+  fn.returned = (expected) => fn.returns.some(returned => deepEqual(expected, returned))
   fn.calledWith = (...expected) => {
     return fn.args.some(args => {
       return expected.every((expectedArg, i) => expectedArg === args[i])
